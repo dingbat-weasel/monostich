8433bcbf0167864885ab297c209d8c8562fd8921
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-06-06 10:59:04 -0700

    added index.js, rename Poem.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,33 +1,4 @@
-const { Schema, model } = require("mongoose");
+const User = require("./User");
+const Poem = require("./Poem");
 
-const poemSchema = new Schema({
-  poemTitle: {
-    type: String,
-    default: "untitled",
-    maxlength: 30,
-    trim: true,
-  },
-  poemText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  poemAuthor: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    // getter that formats date/time with util function
-    // get: (timestamp) => dateFormat(timestamp),
-  },
-  // LIKES: Array of Likes, '[likedBy: user_id, ...]'
-  // COMMENTS: text, author, createdAt
-});
-
-const Poem = model("Poem", poemSchema);
-
-module.exports = Poem;
+module.exports = { User, Poem };
