1e22ebe57fe9f401f4811ec2b5e5648e73e35633
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-06-06 16:12:14 -0700

    working on nav bar, to do: set global state vars to accept dark/light mode

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,93 @@
-import React, { Fragment } from "react";
-import { AppBar, Typography } from "@mui/material";
+// TO DO: Set Global state system
+// - Update onClick for dark/light mode toggle in nav to effect this global state
+
+import React from "react";
+import { useState } from "react";
+import {
+  Box,
+  InputBase,
+  IconButton,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../CSS/FlexBetween";
 
 const Navbar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const navigate = useNavigate();
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+
+  //   Theme Settings
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
   return (
-    <Fragment>
-      <AppBar>
-        <Typography>NavBar</Typography>
-      </AppBar>
-    </Fragment>
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          color="primary"
+          onClick={() => navigate("/")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer",
+            },
+          }}
+        >
+          Monostich
+        </Typography>
+        {isNonMobileScreen && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+
+      {/* Desktop Nav */}
+      {isNonMobileScreen ? (
+        <FlexBetween gap="2rem">
+          {/* TO DO: Light/Dark Mode Toggle */}
+          <IconButton onClick={"#"}>
+            {theme.palette.mode === "dark" ? (
+              <DarkMode sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: "25px" }} />
+            )}
+          </IconButton>
+        </FlexBetween>
+      ) : (
+        <IconButton></IconButton>
+      )}
+    </FlexBetween>
   );
 };
 
