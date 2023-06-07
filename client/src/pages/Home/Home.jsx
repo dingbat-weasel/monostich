c97c2132b508cc8338bc291b74cbb3ddec22fb13
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-06-07 09:58:50 -0700

    grid added homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,42 @@
 import React, { Fragment } from "react";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/Navbar";
+import Main from "./Main";
+import { styled } from "@mui/material/styles";
 
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Box, Container } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <Grid container>
+        {/* Top Spacing */}
+        <Grid item xs={12} md={12}>
+          <Item>xs=12 md=12</Item>
+        </Grid>
+        <Grid container mx={{ xs: 2, md: 6, lg: 10 }}>
+          {/* Main Container */}
+          <Grid item xs={12} md={8}>
+            <Item>xs=12 md=8</Item>
+            <Main />
+          </Grid>
+
+          {/* Sidebar Container */}
+          <Grid item md={4} sx={{ display: { xs: "none", md: "grid" } }}>
+            <Item>xs=0 md=4</Item>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
