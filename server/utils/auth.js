a9dd98cd78006d4cdf538b7b9916ceb1a919f308
Git Admin <thomas.james.1@hotmail.com>
2023-06-07 22:32:29 -0700

    add bcrypt password hashing to auth.js and user.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,1 +1,9 @@
 //test
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+  const hashedPw = await bcrypt.hash(password, 10);
+  return hashedPw;
+};
+
+module.exports = hashPassword;
