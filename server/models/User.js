455f2dbb2b057271fae844a766e883768cb39ba1
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-06-08 21:20:29 -0700

    updating user schema mongoose

--- server/models/User.js
+++ server/models/User.js
@@ -1,38 +1,52 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+// Mongoose automatically generates an _id property to the schema with type ObjectId
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: true,
-    unique: true,
-    // To Do: Decide on standard max/min lengths for user inputs
-    // Declare these requirements to user on client side
-    maxlength: 30,
+    required: [true, "Enter a username."],
+    unique: [true, "That username is taken."],
+    minlength: [3, "Usernames must be more than 3 characters."],
+    maxlength: [36, "Usernames must be less than 36 characters."],
     trim: true,
+    validate: [
+      validator.isAlphnumeric,
+      "Usernames may only include letters and numbers",
+    ],
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: [true, "Enter an email address."],
+    unique: [true, "That email address is taken."],
     // To Do: Improve this match? User email authentication/verification as a sprinkle?
-    match: [/.+@.+\..+/, "Must be a valid email."],
+    validate: [validator.isEmail, "Enter a valid email address."],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 5,
+    required: [true, "Enter a password."],
+    minlength: [8, "Password should be at least eight characters."],
   },
-
-  // TO DO?:  mongoose supports storing arrays and arrays of strings, arrays of other schemas. What is the optimal way to store our poems in the db? https://mongoosejs.com/docs/schematypes.html#arrays
-  // Poems will be saved as an array of strings ["", "", ...], before we were stringify that array to save it as one string then parsing later to use. This maintains the distinct tiles which is needed for styling during client side rendering. Do we still need to stringify and parse? Just some thoughts -isaac
   poems: [
     {
       type: Schema.Types.ObjectId,
       ref: "Poem",
     },
   ],
-  // To Do: Saved Poems functionality, Here is something to start:
+  likedPoems: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Poem",
+    },
+  ],
+  commentedPoems: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Poem",
+    },
+  ],
   savedPoems: [
     {
       type: Schema.Types.ObjectId,
@@ -73,24 +87,6 @@ userSchema.methods.isCorrectPassword = function (password, callback) {
     }
   });
 };
-
-// To Do: BCRYPT PASSWORD
-// pre-save on schema that hashes function
-// This is from activity, needs look-over to make sure its good
-// userSchema.pre('save', async function (next) {
-//     if (this.isNew || this.isModified('password')) {
-//       const saltRounds = 10;
-//       this.password = await bcrypt.hash(this.password, saltRounds);
-//     }
-
-//     next();
-//   });
-
-// To Do: isCorrectPassword schema method
-// Pasted from activity, same thing; needs a closer look-at:
-// userSchema.methods.isCorrectPassword = async function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
 
 const User = model("User", userSchema);
 
