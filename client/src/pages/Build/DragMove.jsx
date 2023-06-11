4f0e8c98bcca5122d1a28a9e714c2d57be8e936e
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-06-10 22:05:25 -0700

    added Dragmove comp wrapper for tiles

---
+++ client/src/pages/Build/DragMove.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { PropTypes } from "prop-types";
+
+export default function DragMove(props) {
+  const {
+    onPointerDown,
+    onPointerUp,
+    onPointerMove,
+    onDragMove,
+    children,
+    style,
+    className,
+  } = props;
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handlePointerDown = (e) => {
+    setIsDragging(true);
+
+    onPointerDown(e);
+  };
+
+  const handlePointerUp = (e) => {
+    setIsDragging(false);
+
+    onPointerUp(e);
+  };
+
+  const handlePointerMove = (e) => {
+    if (isDragging) onDragMove(e);
+
+    onPointerMove(e);
+  };
+
+  useEffect(() => {
+    // Should we apply event listener to tile parent container?
+    window.addEventListener("pointerup", handlePointerUp);
+
+    return () => {
+      window.removeEventListener("pointerup", handlePointerUp);
+    };
+  }, []);
+
+  return (
+    <div
+      onPointerDown={handlePointerDown}
+      onPointerMove={handlePointerMove}
+      style={style}
+      className={className}
+    >
+      {children}
+    </div>
+  );
+}
+
+const { func, element, shape, bool, string } = PropTypes;
+
+DragMove.propTypes = {
+  onDragMove: func.isRequired,
+  onPointerDown: func,
+  onPointerUp: func,
+  onPointerMove: func,
+  children: element,
+  style: shape({}),
+  className: string,
+};
+
+DragMove.defaultProps = {
+  onPointerDown: () => {},
+  onPointerUp: () => {},
+  onPointerMove: () => {},
+};
