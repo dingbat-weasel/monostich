8b7b410446a5d6375a059e946b773713f8c842d1
dingbat-weasel <isaacrperk@gmail.com>
2026-01-09 16:45:21 -0800

    reset selected tiles and log submission payload

--- app/(main)/create/PoemCreator.tsx
+++ app/(main)/create/PoemCreator.tsx
@@ -2,37 +2,37 @@
 import type { Tables } from '@/types/database.types';
 import { useState } from 'react';
 import Tile from './Tile';
+import { Button } from '@/components/ui/button';
 
-type tile = Pick<Tables<'tiles'>, 'id' | 'text'>;
+type Tile = Pick<Tables<'tiles'>, 'id' | 'text'>;
 
 interface PoemCreatorProps {
-  tiles: tile[];
+  tiles: Tile[];
 }
 
 export default function PoemCreator({ tiles }: PoemCreatorProps) {
-  const [tilesOnFridge, setTilesOnFridge] = useState<tile[]>(tiles);
-  const [selectedTiles, setSelectedTiles] = useState<tile[]>([]);
+  const [selectedTiles, setSelectedTiles] = useState<Tile[]>([]);
 
-  function handleAddtile(tile: tile) {
-    // remove from fridge
-    const updatedTilesOnFridge = tilesOnFridge.filter(
-      (tileOnFridge) => tileOnFridge.id !== tile.id
-    );
-    setTilesOnFridge(updatedTilesOnFridge);
+  // whats in selectedTiles? everything else on the fridge
+  const tilesOnFridge = tiles.filter(
+    (tile) => !selectedTiles.some((selected) => selected.id === tile.id)
+  );
 
-    // add to staging
+  function handleAddTile(tile: Tile) {
     setSelectedTiles([...selectedTiles, tile]);
   }
 
-  function handleRemovetile(tile: tile) {
-    // remove from staging
-    const updatedSelectedTiles = selectedTiles.filter(
-      (selectedTile) => selectedTile.id !== tile.id
-    );
-    setSelectedTiles(updatedSelectedTiles);
+  function handleRemoveTile(tile: Tile) {
+    setSelectedTiles(selectedTiles.filter((t) => t.id !== tile.id));
+  }
 
-    // return to fridge
-    setTilesOnFridge([...tilesOnFridge, tile]);
+  function handleReset() {
+    setSelectedTiles([]);
+  }
+
+  function handleSubmit() {
+    const payload = selectedTiles;
+    console.log(payload);
   }
 
   return (
@@ -40,25 +40,32 @@ export default function PoemCreator({ tiles }: PoemCreatorProps) {
       <p className='text-muted-foreground'>
         Database connection successful. Found {tiles?.length} tiles:
       </p>
-      <div className='flex flex-wrap gap-2'></div>
       <div className='grid grid-cols-1 gap-6'>
-        <div className='min-h-20 bg-slate-300 border flex flex-wrap gap-2 p-4 rounded-lg'>
-          Staging
-          {selectedTiles.map((selectedTile) => (
-            <Tile
-              key={selectedTile.id}
-              tile={selectedTile}
-              onClickHandler={() => handleRemovetile(selectedTile)}
-            />
-          ))}
+        <div className='grid grid-cols-1 gap-4 min-h-20 bg-slate-300 border p-4 rounded-lg'>
+          <div className='flex flex-wrap gap-2 border rounded-2xl items-center p-4 min-h-15'>
+            {selectedTiles.map((selectedTile) => (
+              <Tile
+                key={selectedTile.id}
+                tile={selectedTile}
+                onClickHandler={() => handleRemoveTile(selectedTile)}
+              />
+            ))}
+          </div>
+          <div className='flex justify-between px-2'>
+            <Button onClick={handleReset} variant={'destructive'}>
+              Reset
+            </Button>
+            <Button onClick={handleSubmit} variant={'default'}>
+              Submit
+            </Button>
+          </div>
         </div>
         <div className='flex flex-wrap p-4 gap-2 bg-slate-300 min-h-72 rounded-lg'>
-          Fridge
           {tilesOnFridge?.map((tile) => (
             <Tile
               tile={tile}
               key={tile.id}
-              onClickHandler={() => handleAddtile(tile)}
+              onClickHandler={() => handleAddTile(tile)}
             />
           ))}
         </div>
